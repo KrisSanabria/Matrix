5880df69f7234e19de89bfc6fca0f3e725db99ec
Celess <celeste.zuritaet12d1@gmail.com>
2025-05-09 18:28:31 -0300

    Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,34 +1,43 @@
+
 namespace EjemploRelevamiento
 {
+    public enum MetodoPago
+    {
+        Tarjeta,
+        Efectivo,
+        Transferencia
+    }
     public class Cliente
     {
     private string nombreCompleto;
     private string direccion;
     private  int telefono;
     private string correo;
-    private string MetodoDePago;
-    private List<Mascotas>mascotas;
+    private MetodoPago metodoDePago;
+    private List <Mascota> mascotas;
    
-    public Cliente (string nombreCompleto,string direccion, int telefono,string correo,string MetodoDePago)
+    public Cliente (string nombreCompleto,string direccion, int telefono,string correo,MetodoPago metodoDePago)
 {
     this.nombreCompleto = nombreCompleto;
     this.direccion= direccion;
+    Validaciones.ValidarTelefono(telefono);
     this.telefono= telefono;
+    Validaciones.ValidarCorreo(correo);
     this.correo = correo;
-    this.MetodoDePago = MetodoPago;
-    this.Mascotas =new List<Mascotas>();
+    this.metodoDePago = metodoPago;
+    this.Mascotas =new List <Mascota> ();
 } 
     public void MostrarDatos()
     {
-        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Nombre: " + nombreCompleto);
         Console.WriteLine("Dirección: " + direccion);
         Console.WriteLine("Teléfono: " + telefono);
-        Console.WriteLine("Email: " + email);
-        Console.WriteLine("Método de pago: " + metodoPago);
+        Console.WriteLine("Correo: " + correo);
+        Console.WriteLine("Método de pago: " + metodoDePago);
     }
-    public void AgregarMascota(Mascota mascotas)
+    public void AgregarMascota(Mascota mascota)
     {
-        mascotas.Add(mascotas);
+        mascotas.Add(mascota);
     }
 
       public void MostrarMascotas()
@@ -39,7 +48,10 @@ namespace EjemploRelevamiento
         mascota.MostrarInfo();
         }
      }
+
+     public void AgregarTurno(Turno turno)
+     {
+        turnos.Add(turno);
+     }
   }
 }
-
-
