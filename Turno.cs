42c7c3f03e2572f4e7a86905feeaa820fee4ca01
Lujan Ramirez <lujanramirez375@gmail.com>
2025-05-09 12:01:11 -0300

    Update Turno.cs

--- Turno.cs
+++ Turno.cs
@@ -1,1 +1,27 @@
+namespace EjemploRelevamiento
+{
+  public class Turno
+  {
+    private datetime fechaHora;
+    private Servicio servicio;
+    private Mascotas mascotas;
+    private Profesionales profesionales;
+    private estadoTurno estado ;
+    private Modalidad modalidad;
 
+    public Turno (datetime fechaHora, Servicio servicio, Mascotas mascotas, Profesionales profesionales, estadoTurno estado, Modalidad modalidad)
+   {
+      Validar.ValidacionFechaHora(fechaHora,servicio);
+      this.fechaHora = fechaHora;
+      this.servicio  = servicio;
+      this.mascotas = mascotas;
+      Validar.ValidarDisponibilidad(profesionales, servicio);
+      this.profesionales = profesionales,
+
+   }
+    public void MostrarInfo()
+    {
+      Console.WriteLine( "")
+    }
+  }
+}
