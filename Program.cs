627a8781e52902e064cae9dc0f32a3768aec316f
Lujan Ramirez <lujanramirez375@gmail.com>
2025-05-09 12:02:07 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
+using EjemploRelevamiento;
+
+Console.Write("Ingresar nombre completo:");
+string nombreCompleto=Console.ReadLine();
+Console.Write("Ingresar direccion:");
+string direccion=Console.ReadLine();
+Console.Write("Ingresar numero de telefono:");
+int telefono=Console.ReadLine();
+Console.Write("Ingresar el correo:");
+string correo=Console.ReadLine();
+Console.Write("Ingresar el nombre de la mascota:");
+string nombre=Console.ReadLine();
+Console.Write("Ingresar la especie de la mascota:");
+string especie=Console.ReadLine();
+Console.Write("Ingresar la raza de la mascota:");
+string raza=Console.ReadLine();
+Console.Write("Ingresar la edad:");
+int edad=Console.ReadLine();
+Console.Write("Ingresar el peso:");
+double peso=Console.ReadLine();
+Console.Write("Ingresar el historias de Vacunas:");
+string historialVacunas=Console.ReadLine();
+Console.Write("Ingresar el nombre del servicio:");
+string nombre=Console.ReadLine();
+Console.Write("Ingresar la descripcion:");
+string descripcion=Console.ReadLine();
+Console.Write("Ingresar la duracion Aproximada:");
+int duracionAproximada=Console.ReadLine();
+Console.Write("Ingresar el costo:");
+decimal costo=Console.ReadLine();
+
+
 
