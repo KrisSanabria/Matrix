c0001382825e22c9865ab562ed4a36464aad2fc9
Celess <celeste.zuritaet12d1@gmail.com>
2025-05-09 16:32:20 -0300

    Turno.cs

--- Turno.cs
+++ Turno.cs
@@ -1,27 +1,47 @@
+
 namespace EjemploRelevamiento
 {
+    public enum  estadoTurno
+    {
+        Pendiente,
+        Confirmado,
+        Cancelado,
+        Completado
+    }
+    public enum Modalidad
+    {
+        Presencial,
+        Domicilio
+    }
+
   public class Turno
   {
-    private datetime fechaHora;
+    private DateTime fechaHora;
     private Servicio servicio;
-    private Mascotas mascotas;
+    private Mascota mascotas;
     private Profesionales profesionales;
     private estadoTurno estado ;
     private Modalidad modalidad;
 
-    public Turno (datetime fechaHora, Servicio servicio, Mascotas mascotas, Profesionales profesionales, estadoTurno estado, Modalidad modalidad)
+    public Turno (DateTime fechaHora, Servicio servicio, Mascotas mascotas, Profesionales profesionales, estadoTurno estado, Modalidad modalidad)
    {
       Validar.ValidacionFechaHora(fechaHora,servicio);
       this.fechaHora = fechaHora;
       this.servicio  = servicio;
       this.mascotas = mascotas;
       Validar.ValidarDisponibilidad(profesionales, servicio);
-      this.profesionales = profesionales,
+      this.profesionales = profesionales;
 
    }
     public void MostrarInfo()
     {
-      Console.WriteLine( "")
+      Console.WriteLine($"Turno para {mascotas.nombre} - {Servicio.Nombre}");
+      Console.WriteLine($"Fecha y Hora {fechaHora}");
+      Console.WriteLine($"Profesional: {profesionales.nombreCompleto}");
+      Console.WriteLine($"Estado: {estadoTurno}");
+      Console.WriteLine($"Modalidad {modalidad}");
+
     }
   }
 }
+	
