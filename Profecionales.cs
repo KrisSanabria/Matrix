cabdab99bb0b2542e7793cc8eda65c3f596b239d
Lujan Ramirez <lujanramirez375@gmail.com>
2025-05-09 11:09:06 -0300

    Profecionales.cs

--- Profecionales.cs
+++ Profecionales.cs
@@ -1,1 +1,42 @@
+using EjemploRelevamiento;
+   public class Profesional
+{
+    private string nombreCompleto;
+    private string especialidad;
+    private string numeroMatricula;
+    private List <DateTime> disponibilidad;
 
+    public Profesional(string nombre, string especialidad, string numeroMatricula, List<DateTime> disponibilidad)
+    {
+        this.nombreCompleto = nombre;
+        this.especialidad = especialidad;
+        this.numeroMatricula = numeroMatricula;
+        this.disponibilidad = disponibilidad ?? new List<DateTime>();
+    }
+
+  
+    public string Nombre => nombreCompleto;
+    public string Especialidad => especialidad;
+    public string NumeroMatricula => numeroMatricula;
+    public List<DateTime> Disponibilidad => disponibilidad;
+
+    
+    public bool EstaDisponible(DateTime fechaHora)
+    {
+        return disponibilidad.Contains(fechaHora);
+    }
+
+    
+    public void AgregarDisponibilidad(DateTime fechaHora)
+    {
+        if (!disponibilidad.Contains(fechaHora))
+        {
+            disponibilidad.Add(fechaHora);
+        }
+    }
+
+    public void EliminarDisponibilidad(DateTime fechaHora)
+    {
+        disponibilidad.Remove(fechaHora);
+    }
+}
