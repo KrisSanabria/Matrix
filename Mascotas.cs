d3934e0554b1e9803ee989945182e0643fe1209d
Y4nnia <yanina.medinaet12d1@gmail.com>
2025-05-29 23:47:11 -0300

    Mascotas.cs

--- Mascotas.cs
+++ Mascotas.cs
@@ -1,94 +1,57 @@
 
 namespace EjemploRelevamiento
 {
-public class Mascota
-{
-private string nombre;
-private string especie;
-private string raza;
-private int edad;
-private double peso;
-private string historialVacunas;
-private string condicionMedica;
+    public class Mascota
+    {
+        public string Nombre { get; private set; }
+        public string Especie { get; private set; }
+        public string Raza { get; private set; }
+        private int edad;
+        private double peso;
+        public int Edad
+        {
+            get => edad;
+            set
+            {
+                Validaciones.ValidarEdad(value);
+                edad = value;
+            }
+        }
+        public double Peso
+        {
+            get => peso;
+            set
+            {
+                Validaciones.ValidarPeso(value);
+                peso = value;
+            }
+        }
+        public string HistorialVacunas { get; private set; }
+        public string CondicionMedica { get; private set; }
 
-public string Nombre
-{
-get=> nombre ;
-set=> nombre = value;
-}
-public string Especie
-{
-get=> especie;
-set=> especie = value;
-}
-public string Raza
-{
-get=> raza;
-set=> raza = value;
-}
-public int Edad
-{
-get=> edad;
-set
-{
-Validaciones.ValidarEdad(value);
-edad= value;
-}
-}
-public double Peso
-{
-get=> peso;
-set
-{
-Validaciones.ValidarPeso(value);
-peso=value;
-}
-}
-public string HistorialVacunas
-{
-get=> historialVacunas;
-set=> historialVacunas = value;
-}
-public string CondicionMedica
-{
-get=> condicionMedica;
-set=> condicionMedica = value ;
-}
+        public Mascota(string nombre, string especie, string raza, int edad, double peso, string historialVacunas, string condicionMedica)
+        {
+            this.Nombre = nombre;
+            this.Especie = especie;
+            this.Raza = raza;
+            this.Edad = edad;
+            this.Peso = peso;
+            this.HistorialVacunas = historialVacunas;
+            this.CondicionMedica = condicionMedica;
+        }
 
-public Mascota (string nombre, string especie, string raza, int edad, double peso,string historialVacunas, string condicionMedica)
-{
-this.Nombre= nombre;
-this.Especie= especie;
-this.Raza=raza;
-this.Edad=edad;
-this.Peso=peso;
-this.HistorialVacunas=historialVacunas;
-this.CondicionMedica=condicionMedica;
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("------Informacion de la mascota------: ");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Especie: {Especie}");
+            Console.WriteLine($"Raza: {Raza}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Peso: {Peso}");
+            Console.WriteLine($"Vacunas: {HistorialVacunas}");
+            Console.WriteLine($"Condicion Medica: {CondicionMedica}"); 
+        }
+    }
+    
 }
-
-}
-}
-
-
-
-namespace EjemploRelevamiento
-{
-public static class Validaciones
-{
-public static void ValidarEdad(int edad)
-{
-if( edad <0)
-throw new ArgumentException ("La edad debe ser un numero positivo");
-}
-
-public static void ValidarPeso (double peso)
-{
-if (peso <=0)
-throw new ArgumentException ("El peso debe ser mayor a 0");
-}
-}
-}
-
-
-
 
