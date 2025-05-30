2a757f59b50dee936c23ea12ff852d122f4706f6
Celess <celeste.zuritaet12d1@gmail.com>
2025-05-30 14:20:20 -0300

    Validaciones.cs

--- Validaciones.cs
+++ Validaciones.cs
@@ -2,34 +2,34 @@ namespace EjemploRelevamiento
 {
     public static class Validaciones
     {
-       
-        public static void ValidarPeso (double peso)
+
+        public static void ValidarPeso(double peso)
         {
-            if ( peso <= 0)
-            throw new ArgumentException ("El peso debe ser mayor a 0");
+            if (peso <= 0)
+            throw new ArgumentException("El peso debe ser mayor a 0");
         }
-        public static void ValidarTelefono (int telefono)
+        public static void ValidarTelefono(int telefono)
         {
             if (telefono <= 0)
             throw new ArgumentException("El numero de telefono no puede estar vacio ni ser negativo");
         }
-        public static void ValidarCorreo (string correo)
+        public static void ValidarCorreo(string correo)
         {
             if (string.IsNullOrWhiteSpace(correo) || !correo.Contains("@") || !correo.Contains("."))
-            throw new ArgumentException ("El correo electrónico no tiene un formato válido");
+            throw new ArgumentException("El correo electrónico no tiene un formato válido");
 
         }
-        public static void ValidarEdad (int edad)
+        public static void ValidarEdad(int edad)
         {
-            if(edad <0 || edad > 25)
-            throw new ArgumentException ("La edad debe ser entre 0 y 25 años");
+            if (edad < 0 || edad > 25)
+            throw new ArgumentException("La edad debe ser entre 0 y 25 años");
         }
-        public static void ValidarCosto (double costo)
+        public static void ValidarCosto(double costo)
         {
             if (costo <= 0)
             throw new ArgumentException("El costo no puede ser 0");
         }
-      
+
         public static void ValidarFechaHora(DateTime fechaHora, Servicio servicio)
         {
             if (servicio.GetRequiereReservaAnticipada())
@@ -42,11 +42,16 @@ namespace EjemploRelevamiento
         public static void ValidarDisponibilidad(Profesional profesional, DateTime fechaHora)
         {
             if (!profesional.EstaDisponible(fechaHora))
-                throw new ArgumentException("El profesional no está disponible en esa fecha y hora");
+            throw new ArgumentException("El profesional no está disponible en esa fecha y hora");
+        }
+
+        public static void ValidarDuracion(TimeSpan duracion)
+        {
+            if (duracion.TotalMinutes <= 0)
+            throw new ArgumentException("La duración debe ser mayor a 0 minutos.");
         }
 
     }
 }
-
 
 
