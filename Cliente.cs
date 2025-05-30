4bb13609e2673bb8048d21a7f0f1729805abf6c0
Y4nnia <yanina.medinaet12d1@gmail.com>
2025-05-29 23:46:22 -0300

    Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -9,49 +9,59 @@ namespace EjemploRelevamiento
     }
     public class Cliente
     {
-    private string nombreCompleto;
-    private string direccion;
-    private  int telefono;
-    private string correo;
-    private MetodoPago metodoDePago;
-    private List <Mascota> mascotas;
-   
-    public Cliente (string nombreCompleto,string direccion, int telefono,string correo,MetodoPago metodoDePago)
-{
-    this.nombreCompleto = nombreCompleto;
-    this.direccion= direccion;
-    Validaciones.ValidarTelefono(telefono);
-    this.telefono= telefono;
-    Validaciones.ValidarCorreo(correo);
-    this.correo = correo;
-    this.metodoDePago = metodoPago;
-    this.Mascotas =new List <Mascota> ();
-} 
-    public void MostrarDatos()
-    {
-        Console.WriteLine("Nombre: " + nombreCompleto);
-        Console.WriteLine("Dirección: " + direccion);
-        Console.WriteLine("Teléfono: " + telefono);
-        Console.WriteLine("Correo: " + correo);
-        Console.WriteLine("Método de pago: " + metodoDePago);
-    }
-    public void AgregarMascota(Mascota mascota)
-    {
-        mascotas.Add(mascota);
-    }
-
-      public void MostrarMascotas()
-    {
-        Console.WriteLine("Mascotas de " + nombre + ":");
-        foreach (Mascota mascota in mascotas)
+        public string NombreCompleto { get; private set; }
+        public string Direccion { get; private set; }
+        public int Telefono { get; private set; }
+        public string Correo { get; private set; }
+        public MetodoPago MetodoDePago { get; private set; }
+        private List<Mascota> mascotas;
+        private List<Turno> turnos;
+        public Cliente(string nombreCompleto, string direccion, int telefono, string correo, MetodoPago metodoDePago)
         {
-        mascota.MostrarInfo();
+            this.NombreCompleto = nombreCompleto;
+            this.Direccion = direccion;
+            Validaciones.ValidarTelefono(telefono);
+            this.Telefono = telefono;
+            Validaciones.ValidarCorreo(correo);
+            this.Correo = correo;
+            this.MetodoDePago = metodoDePago;
+            this.mascotas = new List<Mascota>();
+            this.turnos = new List<Turno>();
         }
-     }
+        public void MostrarDatos()
+        {
+            Console.WriteLine("-----Informacion del Cliente-----:");
+            Console.WriteLine($"Nombre: {NombreCompleto}");
+            Console.WriteLine($"Dirección: {Direccion}");
+            Console.WriteLine($"Teléfono: {Telefono}");
+            Console.WriteLine($"Correo: {Correo}");
+            Console.WriteLine($"Método de pago: {MetodoDePago}");
+        }
+        public void AgregarMascota(Mascota mascota)
+        {
+            mascotas.Add(mascota);
+        }
 
-     public void AgregarTurno(Turno turno)
-     {
-        turnos.Add(turno);
-     }
+        public void MostrarMascotas()
+        {
+            Console.WriteLine("Mascotas del cliente: ");
+            foreach (Mascota mascota in mascotas)
+            {
+                mascota.MostrarInformacion();
+            }
+        }
+
+        public void AgregarTurno(Turno turno)
+        {
+            turnos.Add(turno);
+        }
+        public void MostrarTurnos()
+        {
+        Console.WriteLine("Turnos del cliente:");
+            foreach (Turno turno i  n turnos)
+            {
+                turno.MostrarInfo();
+            }
+        }
   }
 }
