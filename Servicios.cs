e3984b44ba83612e718d8efc24afdd2e447873ff
Y4nnia <yanina.medinaet12d1@gmail.com>
2025-05-29 23:48:43 -0300

    Servicios.cs

--- Servicios.cs
+++ Servicios.cs
@@ -1,38 +1,38 @@
 
 namespace EjemploRelevamiento
 {
-public class Servicio
-{
-private string nombre;
-private string descripcion;
-private int duracionAproximada; 
-private decimal costo; 
-private bool RequiereReservaAnticipada;
-
-public Servicio(string nombre, string descripcion, int duracionAproximada, decimal costo, bool RequiereReservaAnticipada)
+    public class Servicio
     {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-      
-        Validaciones.ValidarDuracion(duracionAproximada);
-        this.duracionAproximada = duracionAproximada;
-        Validaciones.ValidarCosto(costo);
-        this.costo = costo;
-        this.RequiereReservaAnticipada= RequiereReservaAnticipada;
-    }
+        public string Nombre { get; private set; }
+        public string Descripcion { get; private set; }
+        public TimeSpan Duracion { get; private set; }
+        public double Costo { get; private set; }
+        public bool RequiereReserva { get; private set; }
 
-    public void MostrarInfo()
-    {
-        Console.WriteLine($"Nombre: {nombre}");
-        Console.WriteLine($"Descripción: {descripcion}");
-        Console.WriteLine($"Duración: {duracionAproximada}");
-        Console.WriteLine($"Costo: ${costo}");
-        Console.WriteLine($"¿Requiere reserva anticipada?: {(requiereReservaAnticipada ? "Sí" : "No")}");
-    }
-    public string GetNombre() => nombre;
-    public bool GetRequiereReservaAnticipada() => RequiereReservaAnticipada;
-    public int GetDuracion() => duracionAproximada;
-    public decimal GetCosto() => costo;
-}
+        public Servicio(string nombre, string descripcion, TimeSpan duracion, double costo, bool requiereReserva)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Validaciones.ValidarDuracion(duracion);
+            Duracion = duracion;
+            Validaciones.ValidarCosto(costo);
+            Costo = costo;
+            RequiereReserva = requiereReserva;
+        }
 
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("----- Servicio -----");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Descripción: {Descripcion}");
+            Console.WriteLine($"Duración: {Duracion.TotalMinutes} minutos");
+            Console.WriteLine($"Costo: ${Costo}");
+            Console.WriteLine($"¿Requiere reserva?: {(RequiereReserva ? "Sí" : "No")}");
+        }
+
+        public bool GetRequiereReservaAnticipada()
+        {
+            return RequiereReserva;
+        }
+    }
 }
